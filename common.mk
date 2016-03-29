2d64baa7d43b0502ea7aba282fcc3cee9b4ffbd3
Adrian DC <radian.dc@gmail.com>
2016-03-29 18:45:04 +0200

    build: Add Android AOSP-CAF build support
     * Define BOARD_AOSPCAF_BUILD
     * Additional overlay configurations present in AOSP-CAF
     * Link SnapdragonCamera overlays to Lineage's Snap
    
    Change-Id: I49ec15dddc775ce5d294b33e4846b7382176e402
    Signed-off-by: AdrianDC <radian.dc@gmail.com>

--- common.mk
+++ common.mk
@@ -20,6 +20,9 @@ COMMON_PATH := device/sony/blue-common
 
 # Common specific overlays
 DEVICE_PACKAGE_OVERLAYS += $(COMMON_PATH)/overlay
+ifneq ($(BOARD_AOSPCAF_BASED),)
+DEVICE_PACKAGE_OVERLAYS += $(COMMON_PATH)/overlay-aospcaf
+endif
 ifeq ($(BOARD_AOSP_BASED),)
 DEVICE_PACKAGE_OVERLAYS += $(COMMON_PATH)/overlay-lineage
 endif
