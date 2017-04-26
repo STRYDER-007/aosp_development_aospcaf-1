Manuellama/aosp_development_sony8960_n

build: Add Android AOSP-CAF build support
 * Define BOARD_AOSPCAF_BUILD
 * Additional overlay configurations present in AOSP-CAF
 * Link SnapdragonCamera overlays to Lineage's Snap

Change-Id: I49ec15dddc775ce5d294b33e4846b7382176e402
Signed-off-by: AdrianDC <radian.dc@gmail.com>

AndroidProducts.mk
@@ -1,2 +1,3 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_mint.mk
+    $(LOCAL_DIR)/aosp_mint.mk \
+    $(LOCAL_DIR)/aospcaf_mint.mk

aospcaf_huashan.mk
@@ -1,0 +1,42 @@
+#
+# Copyright (C) 2013-2016 The CyanogenMod Project
+#           (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Device configurations for AOSP-CAF
+BOARD_AOSP_BASED := true
+BOARD_AOSPCAF_BASED := true
+
+# Inherit common AOSP-CAF stuff
+ifneq ($(BOARD_AOSPCAF_BASED),)
+$(call inherit-product-if-exists, vendor/aosp/common.mk)
+endif
+
+# Inherit device configurations
+$(call inherit-product, device/sony/mint/device.mk)
+
+# Device display
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+# Device identifications
+PRODUCT_DEVICE := mint
+PRODUCT_NAME := aospcaf_mint
+PRODUCT_BRAND := Sony
+PRODUCT_MANUFACTURER := Sony
+PRODUCT_MODEL := Xperia T
+
+# Build fingerprints
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LT30p BUILD_FINGERPRINT=Sony/LT30p/LT30p:4.3/9.2.A.1.205/Mvv_tg:user/release-keys PRIVATE_BUILD_DESC="LT30p-user 4.3 JB-MR2-BLUE-140313-0947 257 test-keys"

vendorsetup.sh
@@ -16,3 +16,4 @@
 
 add_lunch_combo aosp_mint-eng
 add_lunch_combo aosp_mint-userdebug
+add_lunch_combo aospcaf_mint-userdebug

STRYDER-007/aosp_development_aospcaf-1

AndroidProducts.mk
@@ -1,2 +1,3 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_hayabusa.mk
+    $(LOCAL_DIR)/aosp_hayabusa.mk \
+    $(LOCAL_DIR)/aospcaf_hayabusa.mk

aospcaf_huashan.mk
@@ -1,0 +1,42 @@
+#
+# Copyright (C) 2013-2016 The CyanogenMod Project
+#           (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Device configurations for AOSP-CAF
+BOARD_AOSP_BASED := true
+BOARD_AOSPCAF_BASED := true
+
+# Inherit common AOSP-CAF stuff
+ifneq ($(BOARD_AOSPCAF_BASED),)
+$(call inherit-product-if-exists, vendor/aosp/common.mk)
+endif
+
+# Inherit device configurations
+$(call inherit-product, device/sony/hayabusa/device.mk)
+
+# Device display
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+# Device identifications
+PRODUCT_DEVICE := hayabusa
+PRODUCT_NAME := aospcaf_hayabusa
+PRODUCT_BRAND := Sony
+PRODUCT_MANUFACTURER := Sony
+PRODUCT_MODEL := Xperia TX
+
+# Build fingerprints
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LT29i BUILD_FINGERPRINT=Sony/LT29i/LT29i:4.3/9.2.A.0.295/2P7_tg:user/release-keys PRIVATE_BUILD_DESC="LT29i-user 4.3 JB-MR2-BLUE-CAF-140119-1326 51 test-keys"

vendorsetup.sh
@@ -16,3 +16,4 @@
 
 add_lunch_combo aosp_hayabusa-eng
 add_lunch_combo aosp_hayabusa-userdebug
+add_lunch_combo aospcaf_hayabusa-userdebug